36a2cb4f73daa87f8184df300361fc1f4abbb4c8
weshy007 <josephwaweru96@gmail.com>
2021-01-01 14:29:40 +0300

    bugs on toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,35 @@
+function feedback() {
+  var nn = document.getElementById("name");
+  var ee = document.getElementById("email");
+  var mm = document.getElementById("message");
+
+  if (mm === "")
+  alert("please enter a message")
+
+  else if (ee === "")
+  alert("please enter an email")
+
+  else if (nn === "")
+  alert("enter your name")
+
+}
+
+
+
+
 $(document).ready(function() {
     $(".design").click(function() {
       $(".design").toggle();
       $(".para1-hidden").toggle();
     });
-  })
+  });
 
 $(".development").click(function() {
-        $("development").toggle();
+        $(".development").toggle();
         $(".para2-hidden").toggle();
 });
 
 $(".product").click(function() {
-    $("product").toggle();
+    $(".product").toggle();
     $(".para3-hidden").toggle();
 });
