e43cdde05803f0c2119f512c47973f29bf26ec7b
weshy007 <josephwaweru96@gmail.com>
2021-01-02 10:17:07 +0300

    design toggle code working

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,27 @@
-function feedback() {
-  var nn = document.getElementById("name");
-  var ee = document.getElementById("email");
-  var mm = document.getElementById("message");
+// function feedback() {
+//   var nn = document.getElementById("name");
+//   var ee = document.getElementById("email");
+//   var mm = document.getElementById("message");
 
-  if (mm === "")
-  alert("please enter a message")
+//   if (mm === "")
+//   alert("please enter a message")
 
-  else if (ee === "")
-  alert("please enter an email")
+//   else if (ee === "")
+//   alert("please enter an email")
 
-  else if (nn === "")
-  alert("enter your name")
+//   else if (nn === "")
+//   alert("enter your name")
+// }
 
-}
+$("form#form").on("submit",function(event) {
+  event.preventDefault();
+  let name = $("input#name").val();
+  // let email = $("input#email").val();
+  // let message = $("input#message").val();
+
+  var results = (name + ", thank you for contacting us");
+  $("#output").text(results); 
+});
 
 
 
@@ -21,6 +30,10 @@ $(document).ready(function() {
     $(".design").click(function() {
       $(".design").toggle();
       $(".para1-hidden").toggle();
+    });
+    $(".para1-hidden").click(function() {
+      $(".para1-hidden").toggle();
+      $(".design").toggle();
     });
   });
 
